484fdeb093e9fc11a3efff5376efdc8203d91737
Lucky Baraka <luckybaraka21@gmail.com>
2026-07-19 14:54:22 +0300

    created the handlers

---
+++ cmd/web/handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from snippetbox"))
+}
+
+// add a snippetView Handler - view  specific response
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Displaying a specific snippet with ID %d...", id)
+}
+
+// create a new snippet
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("creating a new snippet..."))
+}
