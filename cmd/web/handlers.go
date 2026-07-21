6ed36c98596e7f0723a25826a9d7d20b71c57260
Lucky Baraka <luckybaraka21@gmail.com>
2026-07-21 12:24:52 +0300

    added something that could help me work things out well

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,23 +14,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
-	ts, err := template.ParseFiles("./ui/html/home.html")
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+	}
+
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we can pass the slice of file
+	// paths as a variadic parameter?
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server error", http.StatusInternalServerError)
 	}
 
-	err = ts.Execute(w, nil)
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	// w.Write([]byte("Hello from snippetbox"))
+	w.Write([]byte("Hello from snippetbox"))
 }
 
 // add a snippetView Handler - view  specific response
